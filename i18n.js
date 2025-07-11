e7d642e71aa490ae47b90111be23cee343e15134
GitXpedition <feldman.carlos@gmail.com>
2025-07-11 12:51:39 -0300

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -13,32 +13,32 @@ i18n
     },
     resources: {
       es: {
-        translation: {
+       translation: {
           page_status: "Puesta a Punto del Portal",
           attribution_by: "Por",
           pillar1_title: "El ruido se desvanece",
           pillar1_subtitle: "Hasta que solo queda lo importante.",
           pillar2_title: "Lo complejo se vuelve simple",
-          pillar2_subtitle: "Datos clasificados en su escala correcta aseguran la navegación intuitiva.",
+          pillar2_subtitle: "Datos clasificados en su escala correcta aseguran la validación y el contexto.",
           pillar3_title: "Lo esencial, finalmente visible",
           pillar3_subtitle: "Nitidez, sentido y confianza. El portal que preparamos va en esa dirección.",
-          pillar4_title: "Estamos construyendo ese lugar.",
-          pillar4_subtitle: "Muy pronto nuestro servicio de datos va a facilitarte integraciones y negocio."
+          pillar4_title: "Estamos construyendo un servico simple y efectivo.",
+          pillar4_subtitle: "Muy pronto nuestro explorador de eventos va a facilitar el despliegue de certezas."
         }
       },
       en: {
-        translation: {
-          page_status: "Portal Tuning",
-          attribution_by: "By",
-          pillar1_title: "The noise fades away",
-          pillar1_subtitle: "Until only the essential remains.",
-          pillar2_title: "The complex becomes simple",
-          pillar2_subtitle: "Data, classified at the right scale, ensures intuitive navigation.",
-          pillar3_title: "The essential, finally visible",
-          pillar3_subtitle: "Clarity, meaning, and confidence. The portal we are building moves in that direction.",
-          pillar4_title: "We are building that place.",
-          pillar4_subtitle: "Soon, our data service will facilitate your integrations and business needs."
-        }
+translation: {
+  page_status: "Portal Tuning",
+  attribution_by: "By",
+  pillar1_title: "The noise fades away",
+  pillar1_subtitle: "Until only the essential remains.",
+  pillar2_title: "The complex becomes simple",
+  pillar2_subtitle: "Data, classified at the right scale, ensures validation and context.",
+  pillar3_title: "The essential, finally visible",
+  pillar3_subtitle: "Clarity, meaning, and confidence. The portal we are preparing heads in that direction.",
+  pillar4_title: "We are building a simple and effective service.",
+  pillar4_subtitle: "Soon, our event explorer will enable the deployment of certainties."
+}
       }
     }
   });
