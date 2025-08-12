dc09b8b6cf33a3da53f535452dff84ba4d8a0c0f
GitXpedition <feldman.carlos@gmail.com>
2025-08-12 12:08:53 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class', // <--- SE AÑADIÓ ESTA LÍNEA
   theme: {
     extend: {
       backgroundImage: {
