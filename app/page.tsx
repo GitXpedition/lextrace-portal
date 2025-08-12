35c392b295cfd75d7303e71f1fc06860a76b1c04
GitXpedition <feldman.carlos@gmail.com>
2025-08-12 10:55:47 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,21 +34,30 @@ export default function Home() {
           <TranslatedText translationKey="page_status" />&nbsp;
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <div className="flex place-items-center gap-2 p-8 lg:p-0">
-            <TranslatedText translationKey="attribution_by" />{' '}
-            <Image
-              src="/InkTronicBrand.png"
-              alt="Logo inferior"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+          
+          {/* INICIO DE LA MODIFICACIÓN: Se añade el botón de contacto aquí */}
+          <div className="flex items-center gap-4">
+            <a href="mailto:info@lextrace.com" className="rounded-lg border border-transparent px-4 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+              <TranslatedText translationKey="cta_contact" />
+            </a>
+            
+            <div className="flex place-items-center gap-2 p-8 lg:p-0">
+              <TranslatedText translationKey="attribution_by" />{' '}
+              <Image
+                src="/InkTronicBrand.png"
+                alt="Logo inferior"
+                className="dark:invert"
+                width={100}
+                height={24}
+                priority
+              />
+            </div>
           </div>
+          {/* FIN DE LA MODIFICACIÓN */}
         </div>
       </div>
 
-      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative"
           src="/LexTraceGNow.png"
@@ -57,13 +66,6 @@ export default function Home() {
           height={37}
           priority
         />
-        
-        {/* INICIO DE LA MODIFICACIÓN */}
-        <a href="mailto:info@lextrace.com" className="mt-6 text-sm text-gray-400 hover:text-white transition-colors">
-            <TranslatedText translationKey="cta_discover" />
-        </a>
-        {/* FIN DE LA MODIFICACIÓN */}
-
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
